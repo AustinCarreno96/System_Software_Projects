52b1ff9d6b8dc0b194e87000e0f466f6fb445b3a
Austin Carreño <80227315+AustinCarreno96@users.noreply.github.com>
2022-09-09 01:48:08 -0400

    Delete System_Software_Projects directory

--- System_Software_Projects/Project_1_Files/search.h
+++
@@ -1,7 +0,0 @@
-#pragma once // Prevents the header file from loading multiple times
-
-void performSearch(struct student* array[], char* type, char* string);
-
-void performSearch(struct student* array[], char* type, char* string) {
-	
-}
