3716254389b8f93b211603f7f883b1fd74955de1
Austin Carreño <80227315+AustinCarreno96@users.noreply.github.com>
2022-09-21 21:59:23 -0400

    small change to main.

--- Project_2_Files/main.c
+++ Project_2_Files/main.c
@@ -41,6 +41,7 @@ void performPass1(struct symbol* symbolTable[], char* filename, address* address
 
         while((read = getline(&statement, &len, input)) != -1) {			
 			struct segment* temp = prepareSegments(statement);
+			if(temp->first == '#') { continue }
 
 
             memset(statement, 0, INPUT_BUF_SIZE);		// resetting memory at the record array for new record
