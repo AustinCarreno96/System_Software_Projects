bee1418b6eb6867bf7c5a992e6df52a39c7b02b5
Austin Carreño <80227315+AustinCarreno96@users.noreply.github.com>
2022-09-22 22:00:14 -0400

    test

--- Project_2_Files/main.c
+++ Project_2_Files/main.c
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	struct symbol* symbolTable[SYMBOL_TABLE_SIZE] = { NULL };		// Initializing table here.
-	performPass1(symbolTable, argv[1], addresses);
+	performPass1(symbolTable, argv[1], &addresses);
 }
 
 void performPass1(struct symbol* symbolTable[], char* filename, address* addresses) {
